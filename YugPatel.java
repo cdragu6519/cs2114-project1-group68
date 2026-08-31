a21000f82dc347779399c6f25e2caec08d70ff67
yugpatel2026S <yugpatel747@yahoo.com>
2026-08-31 17:26:27 -0400

    Update output message to include 'Yug Patel'

--- YugPatel.java
+++ YugPatel.java
@@ -1,5 +1,5 @@
 public class YugPatel {
     public static void main(String[] args) {
-        System.out.println("Your Name is on the team.");
+        System.out.println("Yug Patel is on the team.");
     }
 }
