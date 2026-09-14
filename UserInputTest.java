84187ba141fce008827fe4a3613eeeaa02572cbe
aurelcatherine <70243301+aurelcatherine@users.noreply.github.com>
2026-09-14 17:59:16 -0400

    Create UserInputTest.java

---
+++ UserInputTest.java
@@ -0,0 +1,87 @@
+package sleepify;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+// -------------------------------------------------------------------------
+/**
+ *  Tests UserInput class
+ * 
+ *  @author catherinearockia
+ *  @version Sep 14, 2026
+ */
+class UserInputTest {
+
+    private UserInput mock;
+    
+    /**
+     * Sets up test cases
+     * 
+     */
+    
+    @BeforeEach
+    public void setUp() {
+        
+        mock = new UserInput(8.5,2,9, "high", "running around in a field of flowers");
+        
+    }
+    
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests getSleepTime() method.
+     */
+    
+    @Test
+    public void testSleepTime() {
+        
+        assertEquals(8.5, mock.getSleepTime(),0.01);
+        System.out.println(mock.getSleepTime());
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests getWokenUp() method.
+     */
+    @Test
+    public void testWokenUp() {
+        
+        assertEquals(2, mock.getWokenUp());
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests getSleepRating() method.
+     */
+    @Test
+    public void testSleepRating() {
+        
+        assertEquals(9, mock.getSleepRating());
+        
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests getEnergy()  method.
+     */
+    @Test
+    public void testEnergy() {
+        
+        assertEquals("high", mock.getEnergy());
+        
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests getDreamLog() method.
+     */
+    @Test
+    public void testDreamLog() {
+        
+        assertEquals("running around in a field of flowers", mock.getDreamLog());
+        
+    }
+}
