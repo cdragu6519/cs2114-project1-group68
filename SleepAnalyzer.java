8c5214ee50f88c869c502b43895d5f40e7031c49
yugpatel2026S <yugpatel747@yahoo.com>
2026-09-20 21:37:45 -0400

    Refactor SleepAnalyzer and improve recommendations
    Refactor SleepAnalyzer methods and fix recommendation logic.

--- SleepAnalyzer.java
+++ SleepAnalyzer.java
@@ -88,11 +88,11 @@ public class SleepAnalyzer
         System.out.println("Lowest sleep day is " + (lowestDay + 1));
     }
 
-    public String reccomendatiionSystem() {
+    public void recommendationSystem() {
         ArrayList<UserInput> entries = sleepData.getEntries();
         
         if(entries.isEmpty()) {
-            return "Not enough data to generate recommendations.";
+            System.out.println("Not enough data to generate recommendations.");
         }
         
         //creating variable for most recent entries 
@@ -100,24 +100,24 @@ public class SleepAnalyzer
        
        if(latestEntry.getSleepTime() < 7 
            && latestEntry.getEnergy().equals("low")) {
-           return "You slept less than 7 hours and reported low enery."
-               + "Try getting more sleep to improve your moood. The recommended"
-               + "amount for sleep is 7-9 hours";
+           System.out.println("You slept less than 7 hours and reported low enery. "
+           + "Try getting more sleep to improve your moood. "
+           + "The recommended amount for sleep is 7-9 hours");
        }
        
        if(latestEntry.getWokenUp() > 3) {
-           return "You woke up several times during the night"
-               + "Consider improving your sleeping enviornment";
+           System.out.println("You woke up several times during the night "
+               + "Consider improving your sleeping enviornment");
        }
        
        if(latestEntry.getSleepTime() >= 7 
            && latestEntry.getEnergy().equals("high")) {
-           return "Your sleep habits appear healthy. Keep it up!";
+           System.out.println("Your sleep habits appear healthy. Keep it up!");
        }
        
        if (latestEntry.getSleepRating() < 5) {
-           return "You rated your sleep poorly" +
-               "Try maintaining a more consistent sleep schedule";
+           System.out.println("You rated your sleep poorly " +
+               "Try maintaining a more consistent sleep schedule");
        }
     }
 }
